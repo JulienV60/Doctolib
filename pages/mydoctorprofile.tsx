752743aaa28e807eb4bc00a15bbe13492fbb8110
Villalpando Julien <villalpando.julien@gmail.com>
2022-04-05 17:30:24 +0200

    fix

--- pages/mydoctorprofile.tsx
+++ pages/mydoctorprofile.tsx
@@ -1,12 +1,12 @@
-import "@progress/kendo-theme-default/dist/all.css";
-
 import BookDrivingSlot from "../components/calendar/Slots_availables";
+import "@progress/kendo-theme-default/dist/all.css";
 import Layout from "../components/Layout";
 function App() {
   return (
     <Layout>
       <div className="App">
         <hr className="k-my-8" />
+        hello
         <BookDrivingSlot />
       </div>
     </Layout>
