dfac5c7185757b42db6bb5df0ce8d344e26e57de
Julien Ferez <Julienferez@icloud.com>
2022-04-05 09:33:56 +0200

    datadoctor

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,149 @@ import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
 import { GetServerSideProps } from "next";
 import { getDatabase } from "../src/database";
+import { Db } from "mongodb";
+import { randomBytes } from "crypto";
 export const getServerSideProps: GetServerSideProps = async () => {
   const mongodb = await getDatabase();
   const test = await mongodb.db().collection("Doctors").find().toArray();
+  const martin = { firsname: "Martin", lastname: "V" };
+  const newDoctor = await mongodb
+    .db()
+    .collection("Doctors")
+    .updateOne(
+      { firsname: "Martin" },
+      {
+        $set: {
+          email: "julienkevin@gmail.com",
+          access_token: "fesfdsfsdf",
+          city: "",
+          speciality: "",
+          slots: {
+            Lundi11: {
+              slot1: {
+                available: false,
+                clientId: "",
+              },
+              slot2: {
+                available: false,
+                clientId: "",
+              },
+              slot3: {
+                available: false,
+                clientId: "",
+              },
+              slot4: {
+                available: false,
+                clientId: "",
+              },
+              slot5: {
+                available: false,
+                clientId: "",
+              },
+            },
+            Mardi12: {
+              slot1: {
+                available: false,
+                clientId: "",
+              },
+              slot2: {
+                available: false,
+                clientId: "",
+              },
+              slot3: {
+                available: false,
+                clientId: "",
+              },
+              slot4: {
+                available: false,
+                clientId: "",
+              },
+              slot5: {
+                available: false,
+                clientId: "",
+              },
+            },
+            Mercredi13: {
+              slot1: {
+                available: false,
+                clientId: "",
+              },
+              slot2: {
+                available: false,
+                clientId: "",
+              },
+              slot3: {
+                available: false,
+                clientId: "",
+              },
+              slot4: {
+                available: false,
+                clientId: "",
+              },
+              slot5: {
+                available: false,
+                clientId: "",
+              },
+            },
+            Jeudi14: {
+              slot1: {
+                available: false,
+                clientId: "",
+              },
+              slot2: {
+                available: false,
+                clientId: "",
+              },
+              slot3: {
+                available: false,
+                clientId: "",
+              },
+              slot4: {
+                available: false,
+                clientId: "",
+              },
+              slot5: {
+                available: false,
+                clientId: "",
+              },
+            },
+            Vendredi15: {
+              slot1: {
+                available: false,
+                clientId: "",
+              },
+              slot2: {
+                available: false,
+                clientId: "",
+              },
+              slot3: {
+                available: false,
+                clientId: "",
+              },
+              slot4: {
+                available: false,
+                clientId: "",
+              },
+              slot5: {
+                available: false,
+                clientId: "",
+              },
+            },
+          },
+        },
+      }
+    );
+
+  console.log("newDoctor", newDoctor);
+
+  // function addNewDoctor(db: Db) {
+  //   db.collection("Doctors").updateOne(
+  //     { _id: "2525" },
+  //     { $set: { email: "JulienKevin@gamil.com" } }
+  //   );
+  // }
+  // addNewDoctor();
+
   return {
     props: {
       data: JSON.stringify(test),
@@ -15,7 +155,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
 };
 const Home: NextPage = ({ data }: any) => {
   const test = JSON.parse(data);
-  console.log(test);
+  console.log("line 18", test);
   return (
     <div>
       <Layout>
