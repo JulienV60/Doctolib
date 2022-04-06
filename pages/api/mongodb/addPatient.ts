1cfc1699333b961d07d48b24804057cb7a100562
Julien Ferez <Julienferez@icloud.com>
2022-04-06 15:36:26 +0200

    en cours form patient

--- pages/api/mongodb/addPatient.ts
+++ pages/api/mongodb/addPatient.ts
@@ -1,4 +1,5 @@
 import { getCookies } from "cookies-next";
+import { ObjectId } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getDatabase } from "../../../src/database";
 
@@ -7,40 +8,65 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "GET" || "POST") {
+    const result = req.body;
     const cookies = { cookie: getCookies({ req, res }) };
+
     const AccessTokenPatient = cookies.cookie.AccessTokenPatient;
+    console.log(cookies);
 
-    const auth0searchUser = await fetch(
-      `https://${process.env.AUTH0_DOMAIN}/userinfo`,
-      {
-        method: "Post",
-        headers: {
-          Authorization: `Bearer ${AccessTokenPatient}`,
-        },
-      }
-    ).then((data) => data.json());
-
-    const mailUserAuth0 = auth0searchUser.email;
+    console.log("AccessTokenPatient", AccessTokenPatient);
+    const idButtonArray = Object.keys(result);
+    const idButton = idButtonArray[0];
+    // console.log(idButton);
     const mongodb = await getDatabase();
-    const newPatient = {
-      category: "Patient",
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: mailUserAuth0,
-    };
-    const searchIfAlreadyhere = await mongodb
+    const searchDb = await mongodb
       .db()
-      .collection("Patients")
-      .findOne({ email: mailUserAuth0 });
+      .collection("Doctors")
+      .findOne({ "Slot.id": new ObjectId(idButton) })
+      .then((data) => data?.Slot);
+    // console.log(searchDb);
+    const Rdv = searchDb.filter((element: any) => {
+      return element.id == idButton;
+    });
 
-    if (searchIfAlreadyhere === null) {
-      const addPatient = await mongodb
-        .db()
-        .collection("Patients")
-        .insertOne(newPatient);
-      res.redirect("/DocAccount");
+    // const cookies = { cookie: getCookies({ req, res }) };
+    // const AccessTokenPatient = cookies.cookie.AccessTokenPatient;
+
+    // const auth0searchUser = await fetch(
+    //   `https://${process.env.AUTH0_DOMAIN}/userinfo`,
+    //   {
+    //     method: "Post",
+    //     headers: {
+    //       Authorization: `Bearer ${AccessTokenPatient}`,
+    //     },
+    //   }
+    // ).then((data) => data.json());
+
+    // const mailUserAuth0 = auth0searchUser.email;
+    // const mongodb = await getDatabase();
+    // const newPatient = {
+    //   category: "Patient",
+    //   firstName: req.body.firstName,
+    //   lastName: req.body.lastName,
+    //   email: mailUserAuth0,
+    // };
+    // const searchIfAlreadyhere = await mongodb
+    //   .db()
+    //   .collection("Patients")
+    //   .findOne({ email: mailUserAuth0 });
+
+    // if (searchIfAlreadyhere === null) {
+    //   const addPatient = await mongodb
+    //     .db()
+    //     .collection("Patients")
+    //     .insertOne(newPatient);
+    //   res.redirect("/PatientForm");
+    // } else {
+    //   res.statu
+    if (AccessTokenPatient !== undefined && Rdv !== null) {
+      res.redirect("/PatientForm");
     } else {
-      res.status(200).redirect("/");
+      res.status(200).redirect("/api/auth/login");
     }
   } else {
     res.statusCode = 405;
