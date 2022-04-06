97463e62442f57187dee1809a5eb188e556dc210
Villalpando Julien <villalpando.julien@gmail.com>
2022-04-06 15:56:10 +0200

    cookie slot

--- pages/api/mongodb/addPatient.ts
+++ pages/api/mongodb/addPatient.ts
@@ -63,6 +63,7 @@ export default async function handler(
     //   res.redirect("/PatientForm");
     // } else {
     //   res.statu
+    console.log(AccessTokenPatient);
     if (AccessTokenPatient !== undefined && Slot !== null) {
       res.redirect("/PatientForm");
     } else {
