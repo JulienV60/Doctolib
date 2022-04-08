0a2b8123df3e7cc1691f340783aa6851b4602689
Julien Ferez <Julienferez@icloud.com>
2022-04-08 10:04:23 +0200

    CSS Page Please Login

--- pages/PleaseLogin.tsx
+++ pages/PleaseLogin.tsx
@@ -3,6 +3,8 @@ import { getDatabase } from "../src/database";
 import React from "react";
 import { getCookies } from "cookies-next";
 import Link from "next/link";
+import LoginIcon from "@mui/icons-material/Login";
+import Layout from "../components/Layout";
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const cookies = { cookie: getCookies({ req, res }) };
   const slot = JSON.stringify(cookies.cookie.Slot);
@@ -17,13 +19,22 @@ export default function pleaseLogin({ data, cookie }: any) {
   const Slot = JSON.parse(cookie);
 
   return (
-    <div>
-      <Link href={`/api/auth/login?slot=${Slot}`}>
-        <a>Plz login</a>
-      </Link>
-      {/* <form action={`/api/auth/login?slot=${Slot}`} method="GET">
+    <Layout>
+      <div className="divPleaseLogin">
+        <h4>You are not logged in, please log in.</h4>
+        <br />
+        <button>
+          <Link href={`/api/auth/login?slot=${Slot}`}>
+            <a>
+              Login as a Patient &nbsp; &nbsp;
+              <LoginIcon />
+            </a>
+          </Link>
+        </button>
+        {/* <form action={`/api/auth/login?slot=${Slot}`} method="GET">
         <button> Plz login</button>
       </form> */}
-    </div>
+      </div>
+    </Layout>
   );
 }
